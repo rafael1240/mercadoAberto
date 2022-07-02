22289efbb13c6023a14a3663baf20dab72402617
rafael <rafael.santos1240@etec.sp.gov.br>
2022-07-02 12:51:33 -0300

    gradient

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Color(0xffffffff),
       appBar: AppBar(
         elevation: 0,
         title: Padding(
@@ -47,18 +47,34 @@ class _MyHomePageState extends State<MyHomePage> {
             icon: Icon(Icons.shopping_cart_outlined,color: Colors.black)
           )
         ],
+
+        bottom: PreferredSize(
+          child: Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 5, 10),
+                child: Icon(Icons.pin_drop,size: 28),
+              ),
+              Text("Enviar para Marcello Queiroz")
+            ],
+          ), 
+          preferredSize: Size.fromHeight(50)
+        ),
       ),
 
-      body: Column(
-        children: [
-          Container(
-            color: Color(0xfff5d415),
-            width: 400,
-            height: 30,
-            child: Text("enviar para"),
+      body: Container(
+        height: 180,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xfff5d415),
+              Color(0xffffffff)
+            ]
           )
-        ],
-      )
+        ),
+      ),
     );
   }
 }
