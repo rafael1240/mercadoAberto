366b2f7832acc35f3a913be82033c8478e739184
Marcello Queiroz <marcellocesar.queiroz@gmail.com>
2022-07-23 02:53:35 -0300

    Adicao de componentes para nivelamento de turma

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-
   final String title;
 
   @override
@@ -32,54 +31,200 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(   
-        backgroundColor: Color(0xfff5d415),
+      appBar: AppBar(
+        toolbarHeight: 86,
         elevation: 0,
-
-        leading: Icon(Icons.menu),
-
-        title: Text(widget.title),
-
-        actions: const [
-          Icon(Icons.shopping_cart_outlined),
-        ],
-
-        bottom: const PreferredSize(
-          preferredSize: Size.fromHeight(50),
-          child: Text("Enviar para Marcello Queiroz - Rua Jardim Paulista, 56"),
-        ),
-
-
-        
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: const FractionalOffset(0.5, 0.2),
-            colors: [
-              Color(0xfff5d415), // Cor amarela
-              Color(0xfff5f5f5), // Cor cinza fundo
-            ]
+        backgroundColor: Color(0xffffdb15),
+        leading: Padding(
+          padding: const EdgeInsets.only(top: 5),
+          child: IconButton(
+            onPressed: () => Scaffold.of(context).openDrawer(),
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black,
+              size: 32,
+            ),
           ),
         ),
-        
+        actions: [
+          Container(
+            width: MediaQuery.of(context).size.width - 110,
+            padding: const EdgeInsets.only(top: 10),
+            child: TextField(
+              onSubmitted: (test) => {},
+              decoration: const InputDecoration(
+                isDense: true,
+                contentPadding: EdgeInsets.only(top: 5),
+                filled: true,
+                fillColor: Colors.white,
+                hintText: "Buscar no Mercado Aberto",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xffffdb15),
+                    width: 0,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(30.0),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xffffdb15),
+                    width: 0,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(30.0),
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xffffdb15),
+                    width: 0,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(30.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(6)),
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Icon(
+              Icons.shopping_cart_outlined,
+              color: Colors.black,
+              size: 32,
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(6)),
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(4.0),
+          child: Container(
+            color: Color(0xffffdb15),
+            height: 40.0,
+            child: Row(
+              children: const [
+                Padding(padding: EdgeInsets.all(5)),
+                Icon(
+                  Icons.pin_drop_outlined,
+                  size: 22,
+                ),
+                Padding(padding: EdgeInsets.all(5)),
+                Text("Enviar para"),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.only(
+          left: 15,
+          right: 15,
+        ),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: FractionalOffset(0.5, 0.2),
+            colors: [
+              Color(0xffffdb15),
+              Color(0xfff5f5f5),
+            ],
+          ),
+        ),
+        child: Column(
+          children: [
+            const Padding(padding: EdgeInsets.all(5)),
+            Container(
+              padding: const EdgeInsets.all(3),
+              height: 180,
+              child: Image.network(
+                'https://http2.mlstatic.com/D_NQ_NP650326-MLA50239834698_062022-B.jpg',
+              ),
+            ),
+            const Padding(padding: EdgeInsets.all(5)),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [Color(0xffaa0f91), Color(0xff171955)]),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.4),
+                    spreadRadius: 0.5,
+                    blurRadius: 2,
+                    offset: const Offset(0, 1.5),
+                  )
+                ],
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    "Assine o nível 6 por R\$ 9,90/mês!",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Icon(
+                    Icons.chevron_right,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+            const Padding(padding: EdgeInsets.all(5)),
+            Container(
+              padding: const EdgeInsets.only(
+                top: 5,
+                bottom: 5,
+              ),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.4),
+                    spreadRadius: 0.5,
+                    blurRadius: 2,
+                    offset: const Offset(0, 1.5),
+                  )
+                ],
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.local_shipping_outlined,
+                    color: Color(0xff43b176),
+                  ),
+                  Text(
+                    " Frete Grátis",
+                    style: TextStyle(
+                      color: Color(0xff43b176),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                    ),
+                  ),
+                  Text(
+                    " em milhões de produtos a partir de RS79",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(padding: EdgeInsets.all(10)),
+          ],
+        ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
